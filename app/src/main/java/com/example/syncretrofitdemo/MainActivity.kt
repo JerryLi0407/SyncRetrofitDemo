5ed27e7860059b0a2944a09f0a46d156b24984f1
Jimmy Li <belled212@gmail.com>
2024-05-10 15:52:29 +0800

    Refactor TaipeiTourData network request for async execution
    - Add error handling with try-catch to catch potential exceptions during network requests.
    - Ensure UI updates are performed on the main thread post-network response to maintain thread safety.

--- app/src/main/java/com/example/syncretrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/syncretrofitdemo/MainActivity.kt
@@ -15,23 +15,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://www.travel.taipei/")
-                .addConverterFactory(GsonConverterFactory.create()) // Gson
-                .build()
+        // 建立 Retrofit 實例的 Builder
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.travel.taipei/")
+            .addConverterFactory(GsonConverterFactory.create()) // Gson
+            .build()
 
-            val apiService = retrofit.create(TaipeiTourApiService::class.java)
-            val response = apiService.getTaipeiTourData().execute()
-            if (response.isSuccessful) {
-                val body = response.body()?.data
-                withContext(Dispatchers.Main) {
-                    val tvData: TextView = findViewById(R.id.tvData)
-                    if (body != null) {
-                        tvData.text = "${body.get(0).name} \n${body.get(0).introduction} \n\n${body.get(0).address} \n${body.get(0).url}"
+        // 通過 Retrofit 實例創建 API 服務接口的實現
+        val apiService = retrofit.create(TaipeiTourApiService::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = try {
+                apiService.getTaipeiTourData().execute()
+            } catch (e: Exception) {
+                null
+            }
+
+            withContext(Dispatchers.Main) {
+                val tvData: TextView = findViewById(R.id.tvData)
+                if (response?.isSuccessful == true) {
+                    // 獲得響應體中的數據部分
+                    val body = response.body()?.data
+                    if (body.isNullOrEmpty()) {
+                        tvData.text = "No Data" // 若無數據，顯示“無數據”
                     } else {
-                        tvData.text = "No Data"
+                        // 若有數據，將數據顯示在 TextView 上
+                        tvData.text = "${body.get(0).name} \n${body.get(0).introduction} " +
+                                "\n\n${body.get(0).address} \n${body.get(0).url}"
                     }
+                } else {
+                    // 若 HTTP 響應失敗，顯示錯誤信息
+                    tvData.text = "Error: ${response?.code()} ${response?.message()}"
                 }
             }
         }
